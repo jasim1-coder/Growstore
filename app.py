8a666e0e5db7cd9b55acbb8d56e7a4319c98823d
Sulav Giri <girishubham77@gmail.com>
2023-03-11 00:11:46 +0545

    api for reco, popular, related and search done

--- app.py
+++ app.py
@@ -1,9 +1,176 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import os
+import sys
+import pandas as pd
+import numpy as np
+import traceback
+import pickle
+import tensorflow as tf
+import json
+from dotenv import load_dotenv, find_dotenv
+from pymongo import MongoClient
+from sklearn.metrics.pairwise import cosine_similarity
+
+load_dotenv(find_dotenv())
+
+database_url = os.environ.get("DATABASE_URL")
 app = Flask(__name__)
+
+client = MongoClient(database_url)
+db = client.MainDB
+
+products_df = pd.read_csv('dataset/cleanedMetaData.csv')
+ratings_df = pd.read_csv('dataset/cleanedRatings.csv')
+with open('trainedModels/svdModel.pkl', 'rb') as f:
+    svdModel = pickle.load(f)
+
+neuralModel = tf.keras.models.load_model('trainedModels/neuralNetwork.h5', compile=False)
+neuralModel.compile(optimizer=tf.optimizers.Adam(learning_rate=0.001), loss='mean_squared_error')
+product_len = products_df.shape[0]
+
+def weighted_rating(row, m, C):
+    # v -> average rating for each item (float)
+    v = row['Number of Rating']
+    # R -> average rating for the item (pd.Series)
+    R = row['Mean Rating']
+    return ( (v / (v + m)) * R) + ( (m / (v + m)) * C )
+
+# Define a function to calculate the fuzzy match score
+def get_match_score(string1, string2):
+    """
+    Computes a similarity score between two strings using the Levenshtein distance algorithm.
+    Returns a float between 0 and 1, where 0 means the strings are completely different and 1 means
+    they are identical.
+    """
+    # Initialize the distance matrix with zeros
+    d = [[0] * (len(string2) + 1) for _ in range(len(string1) + 1)]
+
+    # Populate the matrix with Levenshtein distances
+    for i in range(len(string1) + 1):
+        for j in range(len(string2) + 1):
+            if i == 0:
+                d[i][j] = j
+            elif j == 0:
+                d[i][j] = i
+            elif string1[i - 1] == string2[j - 1]:
+                d[i][j] = d[i - 1][j - 1]
+            else:
+                d[i][j] = 1 + min(d[i][j - 1],        # Insertion
+                                  d[i - 1][j],        # Deletion
+                                  d[i - 1][j - 1])    # Substitution
+
+    # Compute the Levenshtein distance and normalize by the maximum length
+    distance = d[-1][-1]
+    max_length = max(len(string1), len(string2))
+    score = 1 - (distance / max_length)
+
+    return score
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to GrowComers backend'
+
+@app.route('/product/popular', methods = ['GET'])
+def get_popular_products():
+    try:
+        reviewsModel = db.reviews
+        productsModel = db.products
+        ratings = list(reviewsModel.aggregate([
+            {
+                "$project": {
+                    "ProductID": 1,
+                    "Rating": 1,
+                    "UserID": 1
+                }
+            }
+        ]))
+        
+        df = pd.DataFrame.from_records(ratings)
+        df_rating = df.groupby("ProductID").agg({"UserID":"count","Rating":"mean"}).rename(columns={"UserID":"Number of Rating", "Rating":"Mean Rating"}).reset_index()
+        
+        df_filtered=df_rating[df_rating['Number of Rating']>df_rating['Number of Rating'].quantile(q=0.9)]
+        
+        C = df_rating['Mean Rating'].mean()
+        m = df_rating['Number of Rating'].quantile(q=0.9)
+        
+        df_filtered['score']=df_filtered.apply(weighted_rating, m=m, C=C, axis=1)
+        df_highscore=df_filtered.sort_values(by='score', ascending=False).head(10)
+
+        productIds = list(df_highscore.ProductID)
+        
+        products = list(productsModel.find({"_id": {"$in": productIds}}, {"title": 1}))
+        
+        return jsonify(data=products)
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+    
+@app.route('/product/related', methods=['GET'])
+def related_products():
+    try:
+        product_id = request.args.get('id')
+        # Get the product's title, description, and features
+        productsModel = db.products
+        productDetails = list(productsModel.find({"_id": product_id}, {'title': 1, 'description': 1}))
+        product_data = productDetails[0]
+        
+        product_text = product_data['title'] + ' ' + product_data['description'] + ' '
+
+        # Transform the product data using the SVD model
+        product_transformed = svdModel.transform([product_text])
+
+        # Calculate the cosine similarity between the product and all other products in the dataset
+        similarity_matrix = cosine_similarity(product_transformed, svdModel.transform(products_df['title'] + ' ' + products_df['description']))
+
+        # Get the indices of the top 5 most similar products
+        top_indices = similarity_matrix.argsort()[0][-5:][::-1]
+
+        # Get the IDs and titles of the top 5 most similar products
+        top_ids = products_df.loc[top_indices, '_id'].tolist()
+        
+        products = list(productsModel.find({"_id": {"$in": top_ids}}, {"title": 1}))
+            
+        return jsonify(data=products)
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+
+
+# Define recommendation endpoint
+@app.route('/user/recommend', methods=['GET'])
+def recommend():
+    try:
+        productsModel = db.products
+        _id = request.args.get('id')
+        temp_df = ratings_df[ratings_df['UserID'].str.contains(_id)]
+        user_id = temp_df.iloc[0].user
+        product_arr = np.arange(product_len)
+        user = np.array([int(user_id) for _ in range(product_len)])
+        pred = neuralModel.predict([product_arr, user])
+        pred = pred.reshape([product_len])
+        pred_ids = (-pred).argsort()[0:5]
+        prediction_actual_id = ratings_df.loc[ratings_df["product_id"].isin(pred_ids)].reset_index(drop=True)
+        prediction_actual_id_list = list(prediction_actual_id.ProductID.unique())
+        products = list(productsModel.find({"_id": {"$in": prediction_actual_id_list}}, {"title": 1}))
+                
+        return jsonify(data=products)
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+
+@app.route('/product/search', methods=['GET'])
+def searchProduct():
+    query = request.args.get('query')
+    new_df = products_df.copy()
+    
+    new_df['match_score'] = new_df['title'].apply(lambda x: get_match_score(x, query))
+
+    # Sort the dataframe by the match score in descending order
+    new_df = new_df.sort_values('match_score', ascending=False)
+
+    products = new_df[["title", "_id"]].head(10)
+    data = json.dumps(products.to_dict('records'))
+    
+    return data
+    
+
 
 if __name__ == "__main__":
     app.run(port=4002, debug=True)
