492e3fe430d5f28d19a7e98d909c0a00899d0bdb
Sulav Giri <girishubham77@gmail.com>
2023-03-16 23:39:58 +0545

    modified smart seatch to match word by word

--- app.py
+++ app.py
@@ -64,35 +64,44 @@ def listen_for_insert():
 # Function to calculate the fuzzy match score
 
 
-def get_match_score(string1, string2):
+def get_match_score(string, string2):
     """
     Computes a similarity score between two strings using the Levenshtein distance algorithm.
     Returns a float between 0 and 1, where 0 means the strings are completely different and 1 means
     they are identical.
     """
     # Initialize the distance matrix with zeros
-    d = [[0] * (len(string2) + 1) for _ in range(len(string1) + 1)]
+    string = string.lower()
+    string2 = string2.lower()
+    strings = string.split()
+    final_score = -99999
 
-    # Populate the matrix with Levenshtein distances
-    for i in range(len(string1) + 1):
-        for j in range(len(string2) + 1):
-            if i == 0:
-                d[i][j] = j
-            elif j == 0:
-                d[i][j] = i
-            elif string1[i - 1] == string2[j - 1]:
-                d[i][j] = d[i - 1][j - 1]
-            else:
-                d[i][j] = 1 + min(d[i][j - 1],        # Insertion
-                                  d[i - 1][j],        # Deletion
-                                  d[i - 1][j - 1])    # Substitution
+    for string1 in strings:
 
-    # Compute the Levenshtein distance and normalize by the maximum length
-    distance = d[-1][-1]
-    max_length = max(len(string1), len(string2))
-    score = 1 - (distance / max_length)
+        d = [[0] * (len(string2) + 1) for _ in range(len(string1) + 1)]
 
-    return score
+        # Populate the matrix with Levenshtein distances
+        for i in range(len(string1) + 1):
+            for j in range(len(string2) + 1):
+                if i == 0:
+                    d[i][j] = j
+                elif j == 0:
+                    d[i][j] = i
+                elif string1[i - 1] == string2[j - 1]:
+                    d[i][j] = d[i - 1][j - 1]
+                else:
+                    d[i][j] = 1 + min(d[i][j - 1],        # Insertion
+                                      d[i - 1][j],        # Deletion
+                                      d[i - 1][j - 1])    # Substitution
+
+        # Compute the Levenshtein distance and normalize by the maximum length
+        distance = d[-1][-1]
+        max_length = max(len(string1), len(string2))
+        score = 1 - (distance / max_length)
+        if (score > final_score):
+            final_score = score
+
+    return final_score
 
 
 @app.route('/')
