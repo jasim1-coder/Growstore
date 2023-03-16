e69de0bfd5fb9b2bf6456e41f6565f7215b43bec
Sulav Giri <girishubham77@gmail.com>
2023-03-16 23:40:30 +0545

    product search card

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { NODE_API } from "../api/apiIndex";
+import { NODE_API, PYTHON_API } from "../api/apiIndex";
 import Layout from "../components/common/Layout";
+import ProductCard from "../components/product/ProductCard";
 import { getSearchQuery } from "../redux/slice/productSlice";
 
 const ProductListing = () => {
   const query = useSelector(getSearchQuery);
   const [data, setData] = useState([]);
+  const [status, setStatus] = useState("idle");
 
   const fetchProducts = async () => {
     try {
-      const { data } = await NODE_API.get(`/product/search?query=${query}`);
+      setStatus("loading");
+      const { data } = await PYTHON_API.get(`/product/search?query=${query}`);
+      console.log(data);
       setData(data.data);
+      setStatus("idle");
     } catch (error) {
+      setStatus("idle");
       console.log(error);
     }
   };
@@ -24,11 +30,15 @@ const ProductListing = () => {
   return (
     <Layout>
       <div className="container sm:py-12 py-6">
-        {data.length === 0 ? (
-          <p>No product found</p>
-        ) : (
-          data.map((entry, key) => <p key={key}>{entry.title}</p>)
-        )}
+        <div className="grid-list-4 w-full">
+          {status === "loading" ? (
+            <p>Loading...</p>
+          ) : data.length === 0 ? (
+            <p>No product found</p>
+          ) : (
+            data.map((entry, key) => <ProductCard key={key} data={entry} />)
+          )}
+        </div>
       </div>
     </Layout>
   );
