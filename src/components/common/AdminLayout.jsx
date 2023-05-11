3582c604a06b08cae516f8e0e90a399fb4421ac8
Sulav Giri <girishubham77@gmail.com>
2023-05-11 21:59:47 +0545

    admin panel background image

--- src/components/common/AdminLayout.jsx
+++ src/components/common/AdminLayout.jsx
@@ -3,9 +3,11 @@ import AdminSideBar from "./adminsidebar";
 
 const AdminLayout = ({ children }) => {
   return (
-    <div className="flex flex-row justify-start items-start w-full h-full bg-formBackground">
+    <div className="flex flex-row justify-start items-start w-full h-full bg-uiBlueDark">
       <AdminSideBar />
-      <div className="flex-1 min-h-[100vh] sm:p-7 p-3 w-full">{children}</div>
+      <div className="flex-1 min-h-[100vh] sm:p-7 p-3 w-full bg-formBackground">
+        {children}
+      </div>
     </div>
   );
 };
