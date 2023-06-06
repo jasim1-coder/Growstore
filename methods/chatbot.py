eba965dc4e8d402f40f311e2135466ce9cf7163e
Sulav Giri <girishubham77@gmail.com>
2023-06-06 22:37:56 +0545

    ready for deployment

--- methods/chatbot.py
+++ methods/chatbot.py
@@ -13,11 +13,11 @@ from collections import defaultdict
 import datetime
 
 from db import cartsModel, ordersModel, productsModel
-# nltk.download('punkt')
-# nltk.download('wordnet')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('maxent_ne_chunker')
-# nltk.download('words')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
